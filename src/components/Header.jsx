8363c34ae520bcc63aaed4d67ec9426d5c3af3b3
Akbar Rouzi <seagull826@gmail.com>
2024-06-18 23:28:50 -0400

    add header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,15 @@
 export default function Header() {
     return (
         <>
-            <h1>Header</h1>
+            <header id="main-header">
+                <div id="main-title">
+                    <img src="logo.png" alt="Elegant model" />
+                    <h1>Elegant Context</h1>
+                </div>
+                <p>
+                    <button>Cart</button>
+                </p>
+            </header>
         </>
     )
 }
